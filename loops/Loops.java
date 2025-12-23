34978727ea4e3a0a8cc7520c53d20fc9100df00b
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2025-12-23 19:55:43 +0000

    Add examples of while, do-while, and for loops
    Expanded the main method to include demonstrations of while, do-while, and for loops with explanatory comments. These examples illustrate different loop constructs and their use cases in Java.

--- loops/Loops.java
+++ loops/Loops.java
@@ -2,18 +2,34 @@ package loops;
 
 public class Loops {
 
-      public static void main(String a[]){
+      public static void main(String a[]) {
+            // while loop
             int i = 1;
 
-            while (i <= 10){
+            while (i <= 3) {
                   System.out.println("Beko leaning Java " + i);
                   int j = 1;
-                  while (j <= 5){
+                  while (j <= 2) {
                         System.out.println("Inner loop " + j);
                         j++;
                   }
                   i++;
             }
+
+            // do while loop
+            // use case is when you want to execute the loop at least once even in the
+            // condition is false
+            int k = 4;
+
+            do {
+                  System.out.println("Do-While loop iteration " + k);
+                  k++;
+            } while (k <= 3); // here condition is false but loop will execute once
+
+            // for loop
+            for (int f = 0; f <= 6; f += 2) {
+                  System.out.println("Beko learning for loop in Java " + f);
+            }
       }
-      
+
 }
