c2cf56b1951d8da5fd367a1ac4f10850a1918d61
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2025-12-27 18:46:32 +0000

    Update code style comments in Encapsulation.java
    Added clarification on constant naming and camel case usage for multi-word names in the code style comments.

--- encapsulation/Encapsulation.java
+++ encapsulation/Encapsulation.java
@@ -6,9 +6,10 @@ package encapsulation;
 
 // Method names should start with Lowercase letters - getAge(), setName()
 // Variable names should start with Lowercase letters - age, name
-// Constants should be in Uppercase letters with words separated by underscores - PIE, BRAND_NAME
+// Constants should be in Uppercase letters with words separated by underscores - PIE, BRAND_NAME, MAX_VALUE
 
 // Package names should start with Lowercase letters - encapsulation, jagged
+// Camel Case should be used for multi-word name - showMyName(), totalAmount, firstName
 
 class Human {
       private int age;
