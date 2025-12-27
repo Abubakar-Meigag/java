ac52c424d43c7acc430928b3e4428ef8258e5777
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2025-12-27 17:22:31 +0000

    Add Human class with encapsulation example
    Introduces a Human class demonstrating encapsulation with private fields and public getters/setters. Includes a main method to showcase usage.

---
+++ encapsulation/Encapsulation.java
@@ -0,0 +1,34 @@
+
+
+class Human {
+      private int age;
+      private String name;
+
+      public int getAge() {
+            return age;
+      }
+      
+      public String getName() {
+            return name;
+      }
+      
+      public void setAge(int a){
+            age = a;
+      }
+
+      public void setName(String n){
+            name = n;
+      }
+}
+
+public class Encapsulation {
+
+      public static void main(String a[]) {
+
+            Human obj = new Human();
+            obj.setAge(33);
+            obj.setName("Jan");
+
+            System.out.println("My name is: " + obj.getName() + " and I'm " + obj.getAge() + " years old");
+      }
+}
