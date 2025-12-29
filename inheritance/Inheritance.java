072e9a50d7bad7d682395649fe0a9ff1953b3baf
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2025-12-29 15:09:27 +0000

    Add veryAdvCalc class extending AdvCalc with power operation
    extends the inheritance example by adding a veryAdvCalc class that inherits from AdvCalc and implements a powerOfNumber method using Math.pow(). The main method now demonstrates three-level inheritance:
    Calc -> AdvCalc -> veryAdvCalc.

--- inheritance/Inheritance.java
+++ inheritance/Inheritance.java
@@ -4,17 +4,18 @@ public class Inheritance {
 
       public static void main(String a[]){
 
-            AdvCalc obj = new AdvCalc();
+            veryAdvCalc obj = new veryAdvCalc();
 
-            int r1 = obj.add(3, 2);
-            int r2 = obj.sub(6, 5);
-            int r3 = obj.multi(10, 9);
-            int r4 = obj.div(15, 5);
+            int    r1 = obj.add(3, 2);
+            int    r2 = obj.sub(6, 5);
+            int    r3 = obj.multi(10, 9);
+            int    r4 = obj.div(15, 5);
+            double r5 = obj.powerOfNumber(r1, r4);
 
             System.out.println(r1);
             System.out.println(r2);
             System.out.println(r3);
             System.out.println(r4);
+            System.out.println(r5);
       }
-      
 }
---
+++ inheritance/veryAdvCalc.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class veryAdvCalc extends AdvCalc {
+
+      public double powerOfNumber(int n1, int n2){
+
+            return Math.pow(n1, n2);
+      }
+      
+}
